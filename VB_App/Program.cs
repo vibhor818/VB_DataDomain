40c843994b0131c73dcc4c7448101cb93300e439
vibhoraz400 <vibhoraz400@outlook.com>
2023-12-07 17:56:24 +0530

    new change

---
+++ VB_App/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using VB_DataContext.MyContexts;
+using VB_DataDomain.Models;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        using(VBContext con=new VBContext())
+        {
+            var data = con.Emp.ToList();
+
+            foreach(Emp item in data)
+            {
+                Console.WriteLine(item.Id+"  "+item.Name+"  "+item.Address);
+            }
+        }
+    }
+}
---
+++ VB_DataContext/MyContexts/VBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VB_DataDomain.Models;
+
+namespace VB_DataContext.MyContexts
+{
+    public class VBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            optionsBuilder.UseSqlServer(@"data source=(localdb)\mssqllocaldb;database=empdb;trusted_connection=true");
+        }
+        public DbSet<Emp> Emp { get; set; }
+    }
+}
